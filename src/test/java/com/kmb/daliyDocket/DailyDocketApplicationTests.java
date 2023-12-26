f594f7e85301aed7a3d9f358911292fd275a705f
MinBumkim1901 <chngdch3@naver.com>
2023-12-27 01:46:00 +0900

    토이프로젝트

---
+++ src/test/java/com/kmb/daliyDocket/DailyDocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmb.daliyDocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyDocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
