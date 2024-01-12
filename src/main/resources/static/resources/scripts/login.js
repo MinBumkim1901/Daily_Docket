021d14b247f8db54729581c578281fabe4c737ec
MinBumkim1901 <chngdch3@naver.com>
2024-01-12 16:06:27 +0900

    회원가입 성공!! -로그인 기능 제작중-

--- src/main/resources/static/resources/scripts/login.js
+++ src/main/resources/static/resources/scripts/login.js
@@ -10,10 +10,26 @@ loginForm.onsubmit = e => {
 
     if(loginForm['password'].value === ''){
         alert('비밀번호를 입력해주세요');
-        alert('비밀번호를 입력해주세요');
-        alert('비밀번호를 입력해주세요');
         return; //조건문 종료
     }
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+    formData.append('email', loginForm['email'].value);
+    formData.append('password', loginForm['password'].value);
+    xhr.open('POST', `/login`);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                if (xhr.responseText === 'true') {
+                        location.href = '/';
+                } else if(xhr.responseText === 'false') {
+                    alert('로그인이 실패했습니다!');
+                }
+            } else {
+                alert('오류 발생');
+            }
 
-
+        }
+    }
+    xhr.send();
 }
