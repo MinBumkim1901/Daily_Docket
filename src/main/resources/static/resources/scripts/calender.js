1548eed912d6bedc306080748760ceb5d97b0530
MinBumkim1901 <chngdch3@naver.com>
2024-01-17 23:57:24 +0900

    로그인 성공! DB연결해서 일정추가하기!

--- src/main/resources/static/resources/scripts/calender.js
+++ src/main/resources/static/resources/scripts/calender.js
@@ -77,6 +77,7 @@ document.addEventListener('DOMContentLoaded', function () {
         events:[ // 일정 데이터 추가 , DB의 event를 가져오려면 JSON 형식으로 변환해 events에 넣어주면된다.
             {
                 title:'일정DD',
+                title:'일정DD',
                 start:'2021-05-26 00:00:00',
                 end:'2021-05-27 24:00:00'
                 // color 값을 추가해 색상도 변경 가능 자세한 내용은 하단의 사이트 참조
